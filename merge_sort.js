9f4bd100ee359ca68215531f0fedcae82fd6483c
Cuong Le <tekielee@yahoo.com>
2021-04-05 11:51:32 -0700

    improved codes

--- merge_sort.js
+++ merge_sort.js
@@ -1,12 +1,13 @@
-function mergeSort(array) {
-	let len = array.length;
-	let mid = len/2;
-
+function mergeSort(Array) {
+	let len = Array.length;
+	let mid = Math.floor(len/2);
+	
 	if(len === 1) {
-		return array;
+		return Array;
 	}
-	let left = mergeSort(array.splice(0, mid));
-	let right = mergeSort(array);
+	
+	let left = mergeSort(Array.splice(0, mid))
+	let right = mergeSort(Array);
 	return merge(left, right);
 }
 
@@ -15,26 +16,30 @@ function merge(left, right) {
 	let countLeft = left.length;
 	let countRight = right.length;
 	let leftIndex = rightIndex = 0;
+	
 	while(leftIndex < countLeft && rightIndex < countRight) {
 		if(left[leftIndex] > right[rightIndex]) {
 			combined.push(right[rightIndex]);
-			rightIndex++;
+			rightIndex++
 		} else {
 			combined.push(left[leftIndex]);
 			leftIndex++;
 		}
 	}
+	
 	while(leftIndex < countLeft) {
 		combined.push(left[leftIndex]);
 		leftIndex++;
 	}
+	
 	while(rightIndex < countRight) {
 		combined.push(right[rightIndex]);
 		rightIndex++;
 	}
+	
 	return combined;
 }
 
 let array = [20, 45, 93, 67, 10, 97, 52, 88];
 let sortedArray = mergeSort(array);
-console.log(sortedArray);	
+console.log(sortedArray);
